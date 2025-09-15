6740f871a276170af12e60684f040186fb29fa4b
Ritesh-18 <ritsmit1122@gmail.com>
2025-09-15 15:30:18 +0530

    added some more features

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,8 @@ const router=express.Router()
 
 
 
-router.use("/v1" , v1Routes)
+router.use("/v1" , v1Routes) // whenever a url comes as /v1 hit then exexute this V1Routes and same thing you can do with v2 version
+
+
 
 module.exports=router
